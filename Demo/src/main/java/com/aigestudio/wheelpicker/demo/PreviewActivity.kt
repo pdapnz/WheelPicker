af6b7ee109278bad6427f153ec1ba6dc1acc1240
Aige <aigestudio@hotmail.com>
2025-06-27 21:17:20 +0800

    Remove findViewById calling.

--- Demo/src/main/java/com/aigestudio/wheelpicker/demo/PreviewActivity.kt
+++ Demo/src/main/java/com/aigestudio/wheelpicker/demo/PreviewActivity.kt
@@ -4,30 +4,31 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.aigestudio.wheelpicker.WheelPicker
+import com.aigestudio.wheelpicker.demo.databinding.AcPreviewBinding
 
 /**
  * @author AigeStudio 2015-12-06
  * @author AigeStudio 2016-07-08
  */
 class PreviewActivity : AppCompatActivity(), WheelPicker.OnItemSelectedListener {
+    private lateinit var binding: AcPreviewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.ac_preview)
+        binding = AcPreviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val wheelLeft = findViewById<WheelPicker>(R.id.main_wheel_left)
-        wheelLeft.setOnItemSelectedListener(this)
-        val wheelCenter = findViewById<WheelPicker>(R.id.main_wheel_center)
-        wheelCenter.setOnItemSelectedListener(this)
-        val wheelRight = findViewById<WheelPicker>(R.id.main_wheel_right)
-        wheelRight.setOnItemSelectedListener(this)
+        binding.mainWheelLeft.setOnItemSelectedListener(this)
+        binding.mainWheelCenter.setOnItemSelectedListener(this)
+        binding.mainWheelRight.setOnItemSelectedListener(this)
     }
 
     override fun onItemSelected(picker: WheelPicker, data: Any?, position: Int) {
-        var text = ""
-        when (picker.id) {
-            R.id.main_wheel_left -> text = "Left:"
-            R.id.main_wheel_center -> text = "Center:"
-            R.id.main_wheel_right -> text = "Right:"
+        val text = when (picker.id) {
+            binding.mainWheelLeft.id -> "Left:"
+            binding.mainWheelCenter.id -> "Center:"
+            binding.mainWheelRight.id -> "Right:"
+            else -> ""
         }
         Toast.makeText(this, text + data.toString(), Toast.LENGTH_SHORT).show()
     }
